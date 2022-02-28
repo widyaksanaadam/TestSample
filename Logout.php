2767a09e0bf98d2680a3b7aed18cd4209e4346cb
widyaksanaadam <100559444+widyaksanaadam@users.noreply.github.com>
2022-02-28 16:13:43 +0800

    Mencoba Mengedit

--- Logout.php
+++ Logout.php
@@ -1,7 +1,8 @@
 <?php
-    session_start();
     session_unset();
+    session_start();
 
+    echo"<meta http-equiv='refresh' content='0; url=../'>";
     echo"<meta http-equiv='refresh' content='0; url=../'>";
     exit;
 ?>
