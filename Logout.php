53c44fa9e1ddb57ffe84b8e7ed418b8a4f864d71
widyaksanaadam <100559444+widyaksanaadam@users.noreply.github.com>
2022-02-28 16:23:11 +0800

    Update Jadi Login

--- Logout.php
+++ Logout.php
@@ -1,8 +1,19 @@
 <?php
-    session_unset();
     session_start();
+    include"../include/Koneksi.php";
+    $ide = $_POST['username'];
+    $psw = $_POST['password'];
 
-    echo"<meta http-equiv='refresh' content='0; url=../'>";
-    echo"<meta http-equiv='refresh' content='0; url=../'>";
-    exit;
+    $qlogin     = "SELECT * FROM `tb_user` WHERE username='$ide' AND password='$psw'";
+    $datalogin  = mysqli_query($koneksi,$qlogin ) ;
+    $hasil1     = mysqli_fetch_array($datalogin,MYSQLI_ASSOC);
+    $adadata    = mysqli_num_rows($datalogin);
+
+    if ($adadata==1) {
+        $_SESSION['username'] = $hasil1['username'];
+        echo"<meta http-equiv='refresh' content='0; url=../content/edit.php'>";
+    }else {
+        echo"login Gagal Periksa Username Atau Password";
+        echo"<meta http-equiv='refresh' content='2; url=../content/login.php'>";
+    }
 ?>
